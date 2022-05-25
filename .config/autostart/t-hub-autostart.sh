9efcf12d787a6abbb292109e8b8ed94744694ebf
trg <opadamlee@gmail.com>
2022-05-25 16:46:22 -0500

    test

---
+++ .config/autostart/t-hub-autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+[Desktop Entry]
+Type=Application
+Name=t-hub
+Comment=t-hub greeting
+Exec=~/pyvenv/distro-app/code-examples/gtk-with-python/app1.py
+Icon=xfce4-clipman-plugin
+Terminal=false
+Hidden=false
